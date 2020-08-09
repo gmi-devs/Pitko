83d0dfb4d6de0e5d4d8eb1431c25f8af91759402
marko <rekburek@gmail.com>
2020-08-09 16:05:27 +0200

    pitko-content class, ul styles, blog archive img aspect ratio

--- page.php
+++ page.php
@@ -1,7 +1,7 @@
 <?php get_header() ?>
 <?php while ( have_posts() ) : the_post(); ?>
 	<div class="container-1400">
-		<div class="container-680">
+		<div class="container-680 pitko-content">
 			<h1><?php the_title(); ?></h1>
 			<?php the_content(); ?>
 		</div>
